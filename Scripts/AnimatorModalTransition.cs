e254d2f94a52e3b864e8129b34f0ad73f5386570
ddionisio <garapata@gmail.com>
2018-07-11 16:07:55 -0400

    initial commit

---
+++ Scripts/AnimatorModalTransition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using M8.UIModal.Interface;
+
+/// <summary>
+/// Use this for animating open/close of a Modal
+/// </summary>
+public class AnimatorModalTransition : MonoBehaviour, IOpening, IClosing {
+    public M8.Animator.Animate animator;
+
+    public string takeOpen;
+    public string takeClose;
+
+    IEnumerator IOpening.Opening() {
+        if(animator && !string.IsNullOrEmpty(takeOpen)) {
+            animator.Play(takeOpen);
+            while(animator.isPlaying)
+                yield return null;
+        }
+    }
+
+    IEnumerator IClosing.Closing() {
+        if(animator && !string.IsNullOrEmpty(takeClose)) {
+            animator.Play(takeClose);
+            while(animator.isPlaying)
+                yield return null;
+        }
+    }
+}
---
+++ Scripts/AnimatorScreenTransition.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Use this for animating transition between scenes
+/// </summary>
+public class AnimatorScreenTransition : MonoBehaviour, M8.SceneManager.ITransition {
+    public M8.Animator.Animate animator;
+
+    public GameObject activeGO;
+
+    public string takeOut;
+    public string takeIn;
+
+    private int mTakeOutInd;
+    private int mTakeInInd;
+
+    void OnDestroy() {
+        if(M8.SceneManager.instance)
+            M8.SceneManager.instance.RemoveTransition(this);
+    }
+
+    void Awake() {
+        M8.SceneManager.instance.AddTransition(this);
+
+        mTakeOutInd = animator.GetTakeIndex(takeOut);
+        mTakeInInd = animator.GetTakeIndex(takeIn);
+
+        activeGO.SetActive(false);
+    }
+
+    int M8.SceneManager.ITransition.priority { get { return 0; } }
+
+    IEnumerator M8.SceneManager.ITransition.Out() {
+        activeGO.SetActive(true);
+
+        animator.Play(mTakeOutInd);
+        while(animator.isPlaying)
+            yield return null;
+    }
+
+    IEnumerator M8.SceneManager.ITransition.In() {
+        animator.Play(mTakeInInd);
+        while(animator.isPlaying)
+            yield return null;
+        
+        activeGO.SetActive(false);
+
+        animator.ResetTake(mTakeOutInd);
+    }
+}
