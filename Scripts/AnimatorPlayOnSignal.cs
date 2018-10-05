473894eaa7fd7ae1fca5998c2ee8600a47463f2a
ddionisio <garapata@gmail.com>
2018-10-05 18:15:33 -0400

    added AnimatorPlayOnSignal

---
+++ Scripts/AnimatorPlayOnSignal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorPlayOnSignal : MonoBehaviour {
+    public M8.Animator.Animate animator;
+
+    [M8.Animator.TakeSelector(animatorField = "animator")]
+    public string take;
+
+    public bool resetTakeOnEnable;
+
+    public M8.Signal signal;
+
+    private int mTakeInd;
+
+    void OnEnable() {
+        if(resetTakeOnEnable)
+            animator.ResetTake(mTakeInd);
+    }
+
+    void OnDestroy() {
+        signal.callback -= OnSignal;
+    }
+
+    void Awake() {
+        mTakeInd = animator.GetTakeIndex(take);
+
+        signal.callback += OnSignal;
+    }
+
+    void OnSignal() {
+        animator.Play(mTakeInd);
+    }
+}
